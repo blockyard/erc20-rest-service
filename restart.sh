816c9d13b2131b7d20a59cbe5662748c6769e667
qinlk <qinlikang@126.com>
2018-09-09 23:11:40 +0800

    test docker build

--- restart.sh
+++ restart.sh
@@ -1,5 +1,6 @@
 
 docker stop erc20-svc
+docker rm erc20-svc
 docker pull qinlikang/erc20-svc
-docker run -d -p 80:8080 qinlikang/erc20-svc
+docker run -d -p 80:8080 --name erc20-svc qinlikang/erc20-svc
 
